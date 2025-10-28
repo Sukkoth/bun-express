9197e9f8ac8d52e58eddf53599317e08ef4935e8
sukkoth <suukootj@gmail.com>
2025-10-29 00:48:22 +0300

    refactor: refactor user type def and resolvers into another file

---
+++ src/graphql/schema/resolvers/user-resolvers.ts
@@ -0,0 +1,26 @@
+import type { UserRole } from '@/types';
+import * as userService from '@services/user-service';
+import { registerUserSchema } from '@utils/validation/auth';
+import validate from '@utils/validation/validate';
+
+const userResolvers = {
+  Query: {
+    getUserById: async (_: unknown, args: { userId: string }) => {
+      const user = await userService.getUserById(args.userId);
+      return user;
+    },
+  },
+
+  Mutation: {
+    registerUser: async (
+      _: unknown,
+      args: { email: string; name: string; password: string; role?: UserRole },
+    ) => {
+      const data = validate(registerUserSchema, args);
+      const user = await userService.createUser(data);
+      return user;
+    },
+  },
+};
+
+export default userResolvers;
