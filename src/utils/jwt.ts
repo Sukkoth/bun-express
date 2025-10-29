2ad67944912e0f193bdff94cf042842ae750daef
sukkoth <suukootj@gmail.com>
2025-10-29 10:03:34 +0300

    refactor: add payload prop so that jwt is generated using a given payload

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,27 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import Logger from '@libs/logger';
 import { AppException } from '@libs/exceptions/app-exception';
 import { env } from '@libs/configs';
 const { JWT_SECRET } = env;
 
 // Generate a JWT for a user
-export const generateToken = () => {
-  return jwt.sign('', JWT_SECRET, { expiresIn: '10d' });
+export const generateToken = ({
+  payload,
+  expiresIn,
+}: {
+  payload: Record<string, unknown>;
+  expiresIn: SignOptions['expiresIn'];
+}) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn });
 };
 
 // Verify a JWT
-export const verifyToken = (token: string) => {
+export const verifyToken = <T>(token: string): T => {
   try {
-    return jwt.verify(token, JWT_SECRET);
-  } catch (error) {
+    return jwt.verify(token, JWT_SECRET) as T;
+  } catch {
     Logger.error({
       context: 'JWT-VERIFY',
       message: 'Invalid or expired token',
       token,
-      error,
     });
-    throw AppException.unauthenticated({
-      message: 'Unauthenticated',
-    });
+    throw AppException.unauthenticated();
   }
 };
