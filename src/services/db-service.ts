1a430fa8c604b223aa6c8dfe01d7d5e04ade0c60
sukkoth <suukootj@gmail.com>
2025-10-29 10:06:24 +0300

    feat: add getting data via a field

--- src/services/db-service.ts
+++ src/services/db-service.ts
@@ -6,6 +6,17 @@ export async function getById<T>(id: string | number) {
   return result[0];
 }
 
+export async function getByField<T>(
+  table: string,
+  field: string,
+  value: unknown,
+) {
+  const result = await db<
+    T[]
+  >`SELECT * FROM ${sql(table)} WHERE ${sql(field)} = ${value}`;
+  return result;
+}
+
 export async function insert<T>(table: string, data: object) {
   const result = await db<
     T[]
